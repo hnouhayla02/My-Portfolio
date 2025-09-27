64eec02f10d5ab6e359ea7233f95a9974a6046d7
Nouhayla Harchi <155076982+hnouhayla02@users.noreply.github.com>
2025-09-27 22:54:22 +0100

    modified:   src/data/projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,4 @@
-import PetParadise from '../components/petrparadise.png';  
+import PetParadise from '../components/petparadise.png';  
 import Academix from '../components/academix.png';
 import cybersecurity from '../components/cybersecurity.png'; 
 import girl from '../components/girl.png';
